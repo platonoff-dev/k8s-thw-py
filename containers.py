8cb23925b8234af460b9f4ede09f78614edccff7
Anatolii Platonov <platonoff.dev@gmail.com>
2025-10-17 17:05:19 +0400

    Setup network with DNS resolution

--- containers.py
+++ containers.py
@@ -1,18 +1,20 @@
-import asyncio
-import json
-import os
 from pathlib import Path
 
 import podman
-from podman.errors import NotFoundError, ImageNotFound
+from podman.errors import NotFound, ImageNotFound
+from podman.domain.containers import Container
+from podman.domain.images import Image
+from podman.domain.networks import Network
 import yaspin
+
+from podman_client import PodmanClient
 
 
 class ContainerManager:
     def __init__(self, client: podman.PodmanClient):
         self.client = client
 
-    def ensure_node_image(self, image_name: str) -> None:
+    def ensure_node_image(self, image_name: str) -> Image:
         print("Ensuring node images are built")
         try:
             image = self.client.images.get(image_name)
@@ -27,17 +29,62 @@ class ContainerManager:
                 )
 
             print("Node image built successfully.")
+        return image
 
 
     def ensure_node_container(
             self,
             name: str,
             image: str,
-    ) -> None:
+            network: str,
+    ) -> Container:
+        print(f"Ensuring container '{name}' exists")
         try:
             container = self.client.containers.get(name)
-        except NotFoundError:
+        except NotFound:
+            print(f"Container '{name}' not found. Creating")
             container = self.client.containers.create(
                 name=name,
                 image=image,
+                networks={network: {"aliases": [f"{name}.k8s-thw.local"]}},
+                network_mode="bridge",
+                hostname=name,
             )
+        
+        if container.status != "running":
+            print(f"Starting container '{name}'")
+            container.start()
+        
+        return container
+    
+    def ensure_network(self, name: str) -> Network:
+        print(f"Ensuring network '{name}' exists")
+        try:
+            network = self.client.networks.get(name)
+        except NotFound:
+            print(f"Network '{name}' not found. Creating")
+            network = self.client.networks.create(
+                name=name,
+                dns_enabled=True,
+            )
+        
+        return network
+
+    def remove_container(self, name: str) -> None:
+        print(f"Removing container '{name}' if it exists")
+        try:
+            container = self.client.containers.get(name)
+            container.kill()
+            container.remove()
+            print(f"Container '{name}' removed.")
+        except NotFound:
+            print(f"Container '{name}' does not exist. No action taken.")
+    
+    def remove_network(self, name: str) -> None:
+        print(f"Removing network '{name}' if it exists")
+        try:
+            network = self.client.networks.get(name)
+            network.remove()
+            print(f"Network '{name}' removed.")
+        except NotFound:
+            print(f"Network '{name}' does not exist. No action taken.")
--- main.py
+++ main.py
@@ -1,24 +1,55 @@
+import sys
 import podman
 
 import containers
 
 
 def start() -> None:
-    pass
-
-def clean() -> None:
-    pass
-
-def main():
     podman_client =  podman.PodmanClient()
     manager = containers.ContainerManager(podman_client)
-    manager.ensure_node_image("k8s-thw-node")
+    
+    image = manager.ensure_node_image("k8s-thw-node")
+    network = manager.ensure_network("k8s-thw-net")
 
     for i in range(1):
-        pass
+        container = manager.ensure_node_container(
+            name=f"k8s-thw-master-node-{i+1}",
+            image=image.tags[0],
+            network=network.name,
+        )
+    
+    for i in range(3):
+        container = manager.ensure_node_container(
+            name=f"k8s-thw-worker-node-{i+1}",
+            image=image.tags[0],
+            network=network.name,
+        )
 
     podman_client.close()
 
+def clean() -> None:
+    podman_client =  podman.PodmanClient()
+    manager = containers.ContainerManager(podman_client)
+
+    for i in range(1):
+        manager.remove_container(f"k8s-thw-master-node-{i+1}")
+    
+    for i in range(3):
+        manager.remove_container(f"k8s-thw-worker-node-{i+1}")
+    
+    manager.remove_network("k8s-thw-net")
+
+    podman_client.close()
+
+def main():
+    op = sys.argv[1] if len(sys.argv) > 1 else "start"
+    if op == "start":
+        start()
+    elif op == "clean":
+        clean()
+    else:
+        print(f"Unknown operation: {op}")
+        sys.exit(1)
 
 if __name__ == "__main__":
     main()
